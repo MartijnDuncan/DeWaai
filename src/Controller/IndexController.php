c53a21fe460db0811c8e763b52250780992bfeed
martynnvs <57730@student.drenthecollege.nl>
2019-01-16 11:21:31 +0100

    routes enzo

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,15 +1,20 @@
 <?php
-// src/Controller/IndexController.php
+
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController {
+    /**
+     * @Route("/index")
+     */
     public function number() {
         $number = random_int(0,100);
 
         return new Response (
             '<html><body>Lucky number: '.$number.'</body></html>'
             );
-        )
+
     }
 }
