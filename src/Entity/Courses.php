38a8cd8e28669279a621ddd708c1aa236d448eb8
Duncan Spiegelaar <117238@student.drenthecollege.nl>
2019-01-30 15:31:28 +0100

    courses.php

--- src/Entity/Courses.php
+++ src/Entity/Courses.php
@@ -16,8 +16,151 @@ class Courses
      */
     private $id;
 
-    public function getId(): ?int
+    public function getId()
     {
         return $this->id;
     }
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $courseType;
+
+    /**
+     * @return mixed
+     */
+    public function getCourseType()
+    {
+        return $this->courseType;
+    }
+    /**
+     * @ORM\Column(type="integer", length=20)
+     */
+    private $boatType;
+
+    /**
+     * @return mixed
+     */
+    public function getBoatType()
+    {
+        return $this->boatType;
+    }
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $startDate;
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $endDate;
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+    /**
+     * @ORM\Column(type="integer", length=20)
+     */
+    private $price;
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    /**
+     * @ORM\Column(type="integer", length=20)
+     */
+    private $participant;
+
+    /**
+     * @return mixed
+     */
+    public function getParticipant()
+    {
+        return $this->participant;
+    }
+    /**
+     * @ORM\Column(type="integer", length=20)
+     */
+    private $maxparticipants;
+
+    /**
+     * @return mixed
+     */
+    public function getMaxparticipants()
+    {
+        return $this->maxparticipants;
+    }
+
+    /**
+     * @param mixed $courseType
+     */
+    public function setCourseType($courseType): void
+    {
+        $this->courseType = $courseType;
+    }
+
+    /**
+     * @param mixed $boatType
+     */
+    public function setBoatType($boatType): void
+    {
+        $this->boatType = $boatType;
+    }
+
+    /**
+     * @param mixed $startDate
+     */
+    public function setStartDate($startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @param mixed $endDate
+     */
+    public function setEndDate($endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @param mixed $participant
+     */
+    public function setParticipant($participant): void
+    {
+        $this->participant = $participant;
+    }
+
+    /**
+     * @param mixed $maxparticipants
+     */
+    public function setMaxparticipants($maxparticipants): void
+    {
+        $this->maxparticipants = $maxparticipants;
+    }
+
+
 }
