c9c2d43168103f384255868db891062da6899f10
martynnvs <57730@student.drenthecollege.nl>
2019-01-30 14:45:49 +0100

    customer entity created

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -16,8 +16,112 @@ class Customer
      */
     private $id;
 
-    public function getId(): ?int
+    public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="integer", length=6)
+     */
+    private $customerID;
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerID()
+    {
+        return $this->customerID;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstName;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastName;
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function verifyPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=160)
+     */
+    private $address;
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=6)
+     */
+    private $zipcode;
+
+    /**
+     * @return mixed
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * ORM\Column(type="string", length=10)
+     */
+    private $phone;
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $mail;
+
+    /**
+     * @return mixed
+     */
+    public function getMail()
+    {
+        return $this->mail;
     }
 }
