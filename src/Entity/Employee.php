a04faea5d26ef13389c2684f397af3600e94fe0b
Duncan Spiegelaar <117238@student.drenthecollege.nl>
2019-01-31 09:20:00 +0100

    employee.php

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -20,4 +20,105 @@ class Employee
     {
         return $this->id;
     }
+    /**
+     * @ORM\Column(type="integer", length=10)
+     */
+    private $employeeID;
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeeID()
+    {
+        return $this->employeeID;
+    }
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $firstName;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $lastName;
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    /**
+     * @ORM\Column(type="integer", length=20)
+     */
+    private $authLevel;
+
+    /**
+     * @return mixed
+     */
+    public function getAuthLevel()
+    {
+        return $this->authLevel;
+    }
+
+    /**
+     * @param mixed $employeeID
+     */
+    public function setEmployeeID($employeeID): void
+    {
+        $this->employeeID = $employeeID;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param mixed $authLevel
+     */
+    public function setAuthLevel($authLevel): void
+    {
+        $this->authLevel = $authLevel;
+    }
+
 }
